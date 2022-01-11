4ec90e3c745543ba7963108c475d1e7a4d739ab4
Matheus Sousa <mate@pop-os.localdomain>
2022-01-11 03:23:14 -0300

    add: login page

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <LoginTemplate />
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        layout: 'pastelecia',
+    })
+</script>
+
+<style scoped>
+
+</style>
