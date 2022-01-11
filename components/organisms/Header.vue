38324a26a61ccb9d955600564e1bb089d0e89816
Matheus Sousa <mate@pop-os.localdomain>
2022-01-10 23:12:22 -0300

    add: header component

---
+++ components/organisms/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <Container>
+            <Logo/>
+            <Menu/>
+            <Currencies/>
+        </Container>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+.header {
+    .container {
+        width: 100%;
+        height: 80px;
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        grid-template-rows: 1fr;
+        justify-items: center;
+        align-items: center;
+    }
+}
+
+</style>
