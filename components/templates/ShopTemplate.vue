4c2d73ebf1ba72838f640499951de130653a3487
Matheus Sousa <mate@pop-os.localdomain>
2022-01-11 03:22:51 -0300

    add: shop template

---
+++ components/templates/ShopTemplate.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="shop-template">
+        <h1>Cardápio</h1>
+        <Container>
+            <Menu />
+        </Container>
+    </div>
+    
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style lang="scss" scoped>
+
+.shop-template {
+    width: 100%;
+    h1 {
+        text-align: center;
+        color: #000;
+    }
+}
+
+</style>
