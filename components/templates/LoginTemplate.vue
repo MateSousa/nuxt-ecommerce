107314ca090e9a6c642e138340a12a201a3999ce
Matheus Sousa <mate@pop-os.localdomain>
2022-01-11 03:22:39 -0300

    add: login template

---
+++ components/templates/LoginTemplate.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login-template">
+        <h1>Entre na sua conta</h1>
+        <LoginForm/>
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+
+    })
+</script>
+
+<style lang="scss" scoped>
+
+.login-template {
+    width: 100%;
+    h1 {
+        text-align: center;
+    }
+}
+
+</style>
