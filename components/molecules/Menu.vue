57c060f865e05c30466e80e2d1320c6d58c3b9de
Matheus Sousa <mate@pop-os.localdomain>
2022-01-11 03:19:37 -0300

    add: menu component

---
+++ components/molecules/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="menu">
+        hello
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    })
+</script>
+
+<style scoped>
+
+</style>
