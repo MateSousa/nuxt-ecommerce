bd5d51cc87d421c8d465973d311e84d08fb9804c
Mate <matessousa@outlook.com>
2022-04-11 12:00:52 -0300

    update: scss classes

--- components/organisms/ProductItems/Drinks.vue
+++ components/organisms/ProductItems/Drinks.vue
@@ -24,7 +24,7 @@
 
 <style lang="scss" scoped>
 
-.chicken {
+.drinks {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(2, 1fr);
--- components/organisms/ProductItems/Pastry.vue
+++ components/organisms/ProductItems/Pastry.vue
@@ -24,7 +24,7 @@
 
 <style lang="scss" scoped>
 
-.chicken {
+.pastry {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(2, 1fr);
